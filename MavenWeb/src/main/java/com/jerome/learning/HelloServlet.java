94a1d2582b9a3d6efeeea3b6d099cd753261328f
Ning-cloud <536576297@qq.com>
2020-07-01 17:15:50 +0800

    firtst commit

---
+++ MavenWeb/src/main/java/com/jerome/learning/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.jerome.learning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/")
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pw = resp.getWriter();
+		pw.write("<h1>Hello, world!</h1>");
+		pw.flush();
+	}
+}
+
